9cd68470bd5fab76205daeb9b97a92fe64f570ec
jinjinini <jinjinini@naver.com>
2019-11-05 17:13:57 +0900

    메인 메뉴 디자인 및 인터렉션

---
+++ js/webcafe.js
@@ -0,0 +1,22 @@
+var menu_item = $('.menu > li');
+var btn_menu = $('.menu [role = "button"]');
+var sub_link = $('.sub-menu a');
+
+menu_item.addClass('menu-item');
+btn_menu.addClass('btn-menu');
+sub_link.addClass('icon-dot-circled');
+
+btn_menu.click(function(e){
+  var current_class = $(this).parent().hasClass('menu-act')
+  e.preventDefault();
+  menu_item.removeClass('menu-act');
+  // $(this).parent().addClass('menu-act');
+  if(current_class){
+    $(this).parent().removeClass('menu-act');
+  }
+  else{
+    $(this).parent().addClass('menu-act');
+  }
+});
+
+
