6b079874f2a950dd50e88f0897c1b4c0feda1d97
jinjinini <jinjinini@naver.com>
2019-11-07 15:36:21 +0900

    공지사항 및 자료실 마크업과 디자인 및 인터렉션

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,11 +1,17 @@
 var menu_item = $('.menu > li');
 var btn_menu = $('.menu [role = "button"]');
 var sub_link = $('.sub-menu a');
+var tab_list_link = $('.board li a')
+var tab_section = $('.board section')
+var tab = $('.tab a')
 
 menu_item.addClass('menu-item');
 btn_menu.addClass('btn-menu');
 sub_link.addClass('icon-dot-circled');
+tab_list_link.addClass('icon-dot-circled');
 
+
+//메뉴 버튼이벤트
 btn_menu.click(function(e){
   var current_class = $(this).parent().hasClass('menu-act')
   e.preventDefault();
@@ -19,4 +25,10 @@ btn_menu.click(function(e){
   }
 });
 
+//탭 메뉴 버튼이벤트
+tab.click(function(e){
+  e.preventDefault();
+  tab_section.removeClass('tab-act');
+  $(this).parent().parent().addClass('tab-act');
+});
 
